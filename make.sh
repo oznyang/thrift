af9758936e30b22bbea5f58df6ef27da002cccb3
oznyang <oxsean@gmail.com>
2012-11-03 21:29:00 +0800

    add pom and make.sh

---
+++ make.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./bootstrap.sh
+./configure CFLAGS='-O2' CXXFLAGS='-O2' --without-erlang --without-csharp --without-python --without-java --without-php --without-php_extension --without-boost --without-d --without-c_glib --without-ruby --without-haskell --without-go --without-qt4
+make -j8
+make install
